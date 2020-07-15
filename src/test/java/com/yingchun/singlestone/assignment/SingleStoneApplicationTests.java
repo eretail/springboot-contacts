f8872bd0a022426fb8bfacdc545cb10a3f263edd
eretail <yli@DESKTOP-H11EUMA.localdomain>
2020-07-15 10:48:34 -0400

    adding test

--- src/test/java/com/yingchun/singlestone/assignment/SingleStoneApplicationTests.java
+++ src/test/java/com/yingchun/singlestone/assignment/SingleStoneApplicationTests.java
@@ -2,28 +2,63 @@ package com.yingchun.singlestone.assignment;
 
 import java.util.stream.Collectors;
 
+import com.yingchun.singlestone.assignment.repo.ContactRepository;
+import com.yingchun.singlestone.assignment.service.ContactService;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-//@RunWith(SrpingRunner.class)
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SrpingRunner.class)
+@WebMvcTest
 @SpringBootTest
 class SingleStoneApplicationTests {
-/*
-	@Autowired
-	private UserContactService userContactService;
-	
-	@MockBean
-	private UserContactRepository userContactRepository;
-	
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ContactService contactService;
+
+    @MockBean
+    private ContactRepository userContactRepository;
+
+    @Before
+    public void setUp() {
+        Employee alex = new Employee("alex");
+
+        Mockito.when(employeeRepository.findByName(alex.getName()))
+                .thenReturn(alex);
+    }
+
+    @Test
+    public void getContactShouldReturnDetails() {
+        given(this.contactService.schedulePickup(new Date(), new Route());)
+            .willReturn(new Date());
+
+        this.mvc.perform(get("/schedulePickup")
+                .accept(MediaType.JSON)
+                .andExpect(status().isOk());
+    }
+
 	@Test
-	public void getUsersTest() {
+	public void getContactsTest() {
 		when(userContactRepository.findAll()).thenReturn(Stream
 				.of(new Contact(376, "Danile", 31, "USA"), new User(958, "Huy", 35, "UK")).collect(Collectors.toList()));
 		assertEquals(2, service.getUsers().size());
 	}
 
 	@Test
-	public void getUserbyAddressTest() {
+	public void getContactByIdTest() {
 		String address = "Bangalore";
 		when(userContactRepository.findByAddress(address))
 				.thenReturn(Stream.of(new User(376, "Danile", 31, "USA")).collect(Collectors.toList()));
@@ -31,14 +66,14 @@ class SingleStoneApplicationTests {
 	}
 
 	@Test
-	public void saveUserTest() {
+	public void saveContactTest() {
 		User user = new User(999, "Pranya", 33, "Pune");
 		when(userContactRepository.save(user)).thenReturn(user);
 		assertEquals(user, service.addUser(user));
 	}
 
 	@Test
-	public void deleteUserTest() {
+	public void deleteContactByIdTest() {
 		User user = new User(999, "Pranya", 33, "Pune");
 		service.deleteUser(user);
 		verify(userContactRepository, times(1)).delete(user);
@@ -47,7 +82,7 @@ class SingleStoneApplicationTests {
 	@Test
 	void contextLoads() {
 	
-	}*/
+	}
 
 	
 }
